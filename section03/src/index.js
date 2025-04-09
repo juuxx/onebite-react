554177184fd9b77b7f42015f30d9df6d4f6acb71
juuxx <juux_x@naver.com>
2025-04-09 14:38:55 +0900

    randomColor로 라이브러리 import 테스트

--- section03/src/index.js
+++ section03/src/index.js
@@ -1,5 +1,8 @@
 import multi, { add, sub } from "./math.js";
+import randomColor from "randomcolor";
 
-console.log(add(1, 2));
-console.log(sub(1, 2));
-console.log(multi(1, 2));
+const color = randomColor();
+console.log(color);
+// console.log(add(1, 2));
+// console.log(sub(1, 2));
+// console.log(multi(1, 2));
